9aa32fde393978dadc36ea6736ea44f23d2396b5
Sarunya Durai <sarunya.d@samsung.com>
2019-03-12 16:13:49 +0530

    fix api endpoint

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static API_ENDPOINT = 'http://localhost:8080';
+    public static API_ENDPOINT = 'https://merakki.herokuapp.com';
     public static PATHS = {
         products :  {
             get:"/products"
