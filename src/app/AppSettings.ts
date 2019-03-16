1395705a897f078b3bd7a59b6c9fcd802a422ac0
Sarunya Durai <sarunya.d@samsung.com>
2019-03-16 12:56:15 +0530

    point to merakki

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static API_ENDPOINT = 'http://localhost:8080';//'https://merakki.herokuapp.com';
+    public static API_ENDPOINT = 'https://merakki.herokuapp.com';
     public static GOOGLE_API_ENDPOINT = 'https://www.googleapis.com';
     public static PATHS = {
         products :  {
